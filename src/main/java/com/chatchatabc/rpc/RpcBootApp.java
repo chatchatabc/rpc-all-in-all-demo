32570b9eac3c794e60a4369192e8ce705fe7c037
Libing Chen <libing.chen@gmail.com>
2023-01-08 11:50:01 +0800

    app: RpcBootApp

---
+++ src/main/java/com/chatchatabc/rpc/RpcBootApp.java
@@ -0,0 +1,13 @@
+package com.chatchatabc.rpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcBootApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcBootApp.class, args);
+    }
+
+}
