fb7d074b92028c7a07e91adf4ebbba0cbe1344b9
Libing Chen <libing.chen@gmail.com>
2023-01-12 11:59:35 +0800

    refactor: replaced by trpc-spring-boot-starter

--- src/main/java/com/chatchatabc/rpc/application/trpc/TrpcInput.java
+++
@@ -1,9 +0,0 @@
-package com.chatchatabc.rpc.application.trpc;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface TrpcInput {
-}
