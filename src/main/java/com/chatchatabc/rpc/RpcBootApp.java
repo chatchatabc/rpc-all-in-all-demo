b699fca31227a4789789702a6cbf0c5670ebda55
Libing Chen <libing.chen@gmail.com>
2023-05-26 09:24:30 +0800

    feat: add @OpenAPIDefinition

--- src/main/java/com/chatchatabc/rpc/RpcBootApp.java
+++ src/main/java/com/chatchatabc/rpc/RpcBootApp.java
@@ -1,9 +1,23 @@
 package com.chatchatabc.rpc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Spring Boot REST API Documentation",
+                description = "Spring Boot REST API Documentation",
+                version = "0.1.0",
+                contact = @Contact(
+                        name = "linux_china",
+                        email = "libing.chen@gmail.com"
+                )
+        )
+)
 public class RpcBootApp {
 
     public static void main(String[] args) {
